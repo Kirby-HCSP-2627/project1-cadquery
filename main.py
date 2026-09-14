094b08693eb1185d8335bb0a5d733985c5a3ece6
Jose Gomez De La Torre <jgomez@kirby.org>
2026-09-14 14:48:19 -0700

    Modify comments and configure visualization port
    Updated comments and added set_port function for visualization.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # *****Project 1: CadQuery*****
 # Name: 
-# Don't change the next 2 lines! 
+# Don't change the next 4 lines! 
+import os
 import cadquery as cq
-from ocp_vscode import show
+from ocp_vscode import show, set_port
+set_port(3939)
 
 # Example base object code. Run it once to check if everything works! 
 height = 60.0
@@ -12,5 +14,16 @@ thickness = 10.0
 # make the base
 result = cq.Workplane("XY").box(height, width, thickness)
 
-# Render the solid
+
+
+
+
+# Renders the solid
 show(result)
+
+# Uncomment (remove #) the last 5 lines to have your design exported as a STL file in your project folder
+#current_folder = os.path.dirname(os.path.abspath(__file__))
+#export_path = os.path.join(current_folder, 'my_cad_block.stl')
+#print("[*] Exporting model to STL for 3D printing...")
+#cq.exporters.export(result, 'my_cad_block.stl')
+#print("Export complete!")
