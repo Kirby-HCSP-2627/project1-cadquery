96753eee2c703a35655369c36506d8c15c4817f6
Jose Gomez De La Torre <jgomez@kirby.org>
2026-09-14 13:19:08 -0700

    Add project header and instructions to main.py
    Added project header and instructions to main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# *****Project 1: CadQuery*****
+# Name: 
+# Don't change the next 2 lines! 
+import cadquery as cq
+from cadquery.vis import show
+
+# Example base object code. Run it once to check if everything works! 
+height = 60.0
+width = 80.0
+thickness = 10.0
+
+# make the base
+result = cq.Workplane("XY").box(height, width, thickness)
+
+# Render the solid
+show(result)
